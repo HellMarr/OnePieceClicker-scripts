95cbf13093c57512ffda0154d8c68aec2a982178
martin <martin.heliot@free.fr>
2022-08-10 11:59:23 +1000

    First version of the auto clicker
    Signed-off-by: martin <martin.heliot@free.fr>

---
+++ autoclicker.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        [One Piece Clicker] Enhanced Auto Clicker
+// @namespace   One Piece Clicker Scripts
+// @match       https://www.onepiececlicker.com/
+// @grant       none
+// @version     1.0
+// @author      HellMar
+// @description AutoClick for OnePieceClicker.com, works both in dungeons and normal fight
+// ==/UserScript==
+
+function autoClick(){
+
+  if(document.getElementsByClassName("ClickerStyles__CharacterImage-sc-1capclj-2 lihpOl").length>0){
+      let clickZone = document.getElementsByClassName("ClickerStyles__CharacterImage-sc-1capclj-2 lihpOl")[0]
+      clickZone.click();
+  }
+}
+
+window.onload=function(){
+
+  setInterval(autoClick,1);
+}
